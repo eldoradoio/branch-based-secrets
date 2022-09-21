24c755b7709dcb1032980447731716592ffde445
Juan C. Andreu <andreujuanc@gmail.com>
2022-09-21 12:48:58 +0000

    build was not checked in

--- dist/index.js
+++ dist/index.js
@@ -8047,6 +8047,10 @@ function run() {
                 .toUpperCase()
                 .split(",")
                 .map(x => x.trim());
+            const inject = core
+                .getInput("inject", { required: false })
+                .toUpperCase()
+                .trim() == 'TRUE';
             let ref = process.env.GITHUB_REF;
             if (context.eventName == "pull_request") {
                 ref = process.env.GITHUB_BASE_REF;
@@ -8057,10 +8061,19 @@ function run() {
                 environment = 'PROD';
             core.info(`Target branch: ${branch}`);
             secrets.forEach((secret) => {
-                core.exportVariable(`${secret}_NAME`, `${secret}_${environment}`);
+                if (inject) {
+                    core.setSecret(secret);
+                    core.exportVariable(secret, process.env[`${secret}_${environment}`]);
+                }
+                else
+                    core.exportVariable(`${secret}_NAME`, `${secret}_${environment}`);
             });
             envVars.forEach((envVar) => {
-                core.exportVariable(`${envVar}_NAME`, `${envVar}_${environment}`);
+                if (inject) {
+                    core.exportVariable(envVar, process.env[`${envVar}_${environment}`]);
+                }
+                else
+                    core.exportVariable(`${envVar}_NAME`, `${envVar}_${environment}`);
             });
             core.exportVariable("TARGET_ENVIRONMENT", environment);
             core.exportVariable("TARGET_BRANCH", branch);
